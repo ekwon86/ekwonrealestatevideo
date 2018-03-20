2c633ed4c035f4a9778d0851629261dc6fcfc331
Eugene Kwon <ekwon86@gmail.com>
2018-03-20 09:05:36 -0700

    working on about subsections

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,8 +9,10 @@
         <div class="container" id="about-info">
             <div class="row about-us-containers">
                 <div class="col about-us-text-container">
-                    <p class="about-headings">What we do</p>
-                    <p class="about-text">Our specialty is real estate videography that show off spaces and experiences. Our videos are an indispensable marketing tool to help real estate agents sell more homes and business owners attract more customers.  We focus on producing stunning visual content that captures attention & engages consumers.</p>
+                    <div class="about-us-text-subcontainer absolute-right">
+                        <p class="about-headings">What we do</p>
+                        <p class="about-text">Our specialty is real estate videography that show off spaces and experiences. Our videos are an indispensable marketing tool to help real estate agents sell more homes and business owners attract more customers.  We focus on producing stunning visual content that captures attention & engages consumers.</p>
+                    </div>
                 </div>
                 <div class="col about-us-img-container">
                     <img src="../assets/sold.jpg" alt="sold">
@@ -21,14 +23,18 @@
                     <img src="../assets/drone.jpeg" alt="drone">
                 </div>
                 <div class="col about-us-text-container">
-                    <p class="about-headings">Affordability without sacrificing quality</p>
-                    <p class="about-text">Everyone should be proud of the work they do. That’s why at One Shot Productions, quality is never sacrificed. With hand-picked royalty free tracks, creative videography, and Hollywood-grade equipment and training, our FAA 107 licensed, insured, and certified teams are guaranteed to provide your listing with the best marketing materials you’ve ever used, and you don’t have to spend a fortune to get your hands on them.</p>
+                    <div class="about-us-text-subcontainer absolute-left">
+                        <p class="about-headings">Affordability without sacrificing quality</p>
+                        <p class="about-text">Everyone should be proud of the work they do. That’s why at One Shot Productions, quality is never sacrificed. With hand-picked royalty free tracks, creative videography, and Hollywood-grade equipment and training, our FAA 107 licensed, insured, and certified teams are guaranteed to provide your listing with the best marketing materials you’ve ever used, and you don’t have to spend a fortune to get your hands on them.</p>
+                    </div>
                 </div>
             </div>
             <div class="row about-us-containers">
                 <div class="col about-us-text-container">
-                    <p class="about-headings">Complete customer satisfaction</p>
-                    <p class="about-text">Your success is our success and your satisfaction is our utmost is of utmost importance to us.</p>
+                    <div class="about-us-text-subcontainer absolute-right">
+                        <p class="about-headings">Complete customer satisfaction</p>
+                        <p class="about-text">Your success is our success and your satisfaction is our utmost is of utmost importance to us.</p>
+                    </div>
                 </div>
                 <div class="col about-us-img-container">
                     <img src="../assets/editing.jpg" alt="edit">
@@ -47,7 +53,7 @@
     }
     .about-us-containers {
         height: 350px;
-        margin-top: 100px;
+        margin-top: 50px;
         /*border: 1px solid black;*/
     }
     .about-headings {
@@ -62,12 +68,24 @@
     .about-us-text-container, .about-us-img-container {
         height: 100%;
     }
-    .about-us-text-container {
-        
+    .about-us-text-subcontainer {
+        padding: 25px;
+        position: absolute;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        top: 50%;
+        background-color: rgba(255,255,255,0.9);
+        z-index: 9999;
+        transform: translateY(-50%);
+    }
+
+    .absolute-right {
+        right: -75px;
+    }
+    .absolute-left {
+        left: -75px;
     }
 
     .about-us-img-container img {
-        box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
         height: 100%;
         width: 100%;
     }
